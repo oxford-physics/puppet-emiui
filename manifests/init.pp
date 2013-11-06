4dfc48b9b0f27abf69ef985040546f47e227a96d
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2013-11-06 11:59:22 +0000

    Functionality moved to egi_trustanchors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class emiui {
-   $packages = [ "ca-policy-egi-core", "emi-ui", "ca-policy-lcg" ]
+   $packages = [ "emi-ui"  ]
    ensure_packages ( [ $packages ] )
    class {"yaim_conf" : service_type=>"emiui" }
 #   class {"yaim_conf::ui_site_info": }
