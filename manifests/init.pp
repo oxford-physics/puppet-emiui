2088e17c505096b8bc837ef2bc5c79ef70cc243f
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2015-09-14 16:00:18 +0100

    Upstream package deps changed, module now reflects that

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class emiui {
-   $packages = [ "emi-ui"  ]
+   $packages = [ "gridsite-commands", "emi-ui"  ]
    ensure_packages ( [ $packages ] )
    class {"yaim_conf" : service_type=>"emiui" }
 #   class {"yaim_conf::ui_site_info": }
